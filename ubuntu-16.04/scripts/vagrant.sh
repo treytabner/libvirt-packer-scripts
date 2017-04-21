b49553fc62c0add13e103509e977fb18d97d2343
Trey Tabner <awtabner@us.ibm.com>
2017-04-21 11:11:09 -0500

    Add docker key and package

--- ubuntu-16.04/scripts/vagrant.sh
+++ ubuntu-16.04/scripts/vagrant.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Store build time
-date > /etc/vagrant_box.build_time
+#date > /etc/vagrant_box.build_time
 
 # Set up sudo
 echo 'vagrant ALL=NOPASSWD:ALL' > /etc/sudoers.d/vagrant
@@ -16,8 +16,15 @@ sed -i "/[# ]*UseDNS[ \t].*/d" /etc/ssh/sshd_config
 echo "UseDNS no" >> /etc/sshd_config
 
 # NFS used for file syncing
-apt-get --yes install nfs-common
+#apt-get --yes install nfs-common
 
 # Re-enable root user and set password to 'vagrant'
 printf "vagrant\nvagrant\n" | passwd root
 passwd -u root
+
+
+
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | tee /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get install -y docker-engine
